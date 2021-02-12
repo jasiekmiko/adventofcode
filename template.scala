c8ed66a8974089aadc118b696bf79971b7b513ff
Jan Mikołajczak <jasiek.miko@gmail.com>
2021-02-12 11:45:45 +0100

    Add problem template

---
+++ template.scala
@@ -0,0 +1,21 @@
+import scala.io.Source
+
+object Problem {
+	def main(args: Array[String]): Unit {
+		val lines = Source.fromFile(args(1)).getLines()
+
+		val answer = args(0) match {
+			case "part1" => part1(lines)
+			case "part2" => part2(lines)
+		}
+		println(answer)
+	}
+ 
+	def part1(lines: Iterator[String]) = {
+		???
+	}
+
+	def part2(lines: Iterator[String]) = {
+		???
+	}
+}
